d4db37e58c7204f7cf39b563a518d1c11ac983ca
AlexTimchenko <super.oleksiy.timchenko@ukr.net>
2017-12-20 13:17:10 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;(function($){
+    "use srict";
+  	var teamSlider = $('.ba-our-slider');
+  	teamSlider.slick({
+  		dots: true,
+  		slide: '.ba-slide',
+  		prevArrow: '.ba-slider-prev',
+  		nextArrow: '.ba-slider-next',
+  		slidesToShow: 3
+  	});
+
+})(jQuery);
